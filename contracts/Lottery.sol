03154f2735451bdade4d44713754151de14c0617
Lucas <lucasalonso11800@gmail.com>
2021-11-04 22:33:15 -0300

    Basic lottery contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Lottery {
+    address public manager;    
+    address[] public players;
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        players.push(msg.sender);
+    }
+}
